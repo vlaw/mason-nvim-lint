91b3a48b02b53406d4a92837ffac19f2668366de
luowentao <luowentao@meituan.com>
2024-07-05 17:22:12 +0800

    filter out ignore from ensure

--- lua/mason-nvim-lint/settings.lua
+++ lua/mason-nvim-lint/settings.lua
@@ -8,6 +8,10 @@ local DEFAULT_SETTINGS = {
     ---@type string[]
     ensure_installed = {},
 
+    ---@type string[]
+    -- just like `zapling/mason-conform.nvim`, this option allows you to ignore certain linters from being installed.
+    ignore_install = { },
+
     -- Whether linters that are set up (via nvim-lint) should be automatically installed if they're not already installed.
     -- It tries to find the specified linters in the mason's registry to proceed with installation.
     -- This setting has no relation with the `ensure_installed` setting.
@@ -19,11 +23,24 @@ local DEFAULT_SETTINGS = {
 }
 
 M._DEFAULT_SETTINGS = DEFAULT_SETTINGS
+---@class MasonNvimLintSettings
 M.current = M._DEFAULT_SETTINGS
 
 ---@param opts MasonNvimLintSettings
 function M.set(opts)
     M.current = vim.tbl_deep_extend("force", M.current, opts)
+    local ensure_installed = M.current.ensure_installed
+    local ignore_install = M.current.ignore_install
+
+    -- Remove ignored linters from the `ensure_installed` list.
+    if #ensure_installed > 0 then
+        for index, linter in ipairs(ensure_installed) do
+            if vim.list_contains(ignore_install, linter) then
+                table.remove(ensure_installed, index)
+            end
+        end
+    end
+    M.current.ensure_installed = ensure_installed
 end
 
 return M
